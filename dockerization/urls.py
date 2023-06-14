6fb787d92af60f17436a19d66dc812bfadea883c
safwantaliparamba <safwanp2420@gmail.com>
2023-06-14 10:03:07 +0530

    volume managed

--- dockerization/urls.py
+++ dockerization/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings as SETTINGS
 
 
 urlpatterns = [
+    path("", include("web.urls")),
     path('admin/', admin.site.urls),
 ]
 
